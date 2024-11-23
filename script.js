15bad4ff7ae35b90a1ffde37eac74acc58244c1c
Aleem24 <md.samiulaleem16@gmail.com>
2024-11-23 20:34:28 +0600

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-const text = document.getElementById("dom").innerHTML;
-let ans= prompt ("This first header is:");
-text = ans;
+// const text = document.getElementById("dom").innerHTML;
+// let ans= prompt ("This first header is:");
+// text = ans;
+
+
+
+
+const validate(e){
+    e.preventDefault();
+    const email= document.getElementById("email").value;
+
+    const password= document.getElementById("password").value;
+
+    const age= document.getElementById("age").value;
+
+      const msgBox = document.getElementById("msg");
+
+    let message ="Please enter an Email";
+
+      if (email ===""){
+        message = "Please enter an Email";
+        msgBox.style.color = "Orange";
+
+
+      } else if(password ===""){
+        message = "Please enter your password";
+        msgBox.style.color = "Yellow";
+
+        
+      } else if (age ===""){
+        message = "Age must be between 15 to 90";
+        msgBox.style.color = "Violet";
+      } else{
+        message = "Login Successfull!!";
+      };
+      msgBox.innerText = message ;
+
+
+}
